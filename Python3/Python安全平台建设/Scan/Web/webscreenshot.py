001273c2c975fef5fcefb54a7a16c45ccf7ee40c
WeiyiGeek <1564362804@qq.com>
2019-07-31 17:54:56 +0800

    git pycurl

---
+++ Python3/Python安全平台建设/Scan/Web/webscreenshot.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File : webscreenshot.py
+# @CreateTime : 2019/7/31 17:34
+# @Author : WeiyiGeek
+# @Function : 
+# @Software: PyCharm
---
+++ Python3/Python自动化运维/Day4/pycurldemo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File : pycurldemo.py
+# @CreateTime : 2019/7/31 15:23
+# @Author : WeiyiGeek
+# @Function : 
+# @Software: PyCharm
